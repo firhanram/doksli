132cb6c8c6b969f11e570618d2362063d8d9be7a
Firhan Ramadhan <rfirhan99@gmail.com>
2026-03-16 20:41:49 +0700

    Initial Commit

---
+++ doksli/doksliApp.swift
@@ -0,0 +1,17 @@
+//
+//  doksliApp.swift
+//  doksli
+//
+//  Created by Firhan Ramadhan on 16/03/26.
+//
+
+import SwiftUI
+
+@main
+struct doksliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
